3f32aef3480dc9f15efed4e9f34cb60a409e6863
Gaurav <gaurav@192.168.1.30>
2026-05-12 18:46:50 +0530

    Fixed production API and CORS

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "https://cybernova-security.vercel.app"
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use(
